658e3f5d9c4dee2bcc61237e52e3478245dea52b
Stephen J. Bush <muppetjones@gmail.com>
2021-05-12 10:57:36 -0500

    disabled LH outer column

--- keyboards/lily58/keymaps/muppetjones/keymap.c
+++ keyboards/lily58/keymaps/muppetjones/keymap.c
@@ -58,10 +58,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_CLMK_DH] = LAYOUT( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,   KC_0,    KC_BSPC, \
-  KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC, \
-  HY_ESC,  HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,                     KC_M,    HOME_N,  HOME_E,  HOME_I, HOME_O,  KC_QUOT, \
-  KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   RAISE,   LOWER,   KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT, \
-                             KC_LALT, KC_LGUI, LOWER,  KC_SPC,  KC_SPC,  RAISE,   KC_LCTL, KC_RGUI \
+  XXXXXXX, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                     KC_J,    KC_L,    KC_U,    KC_Y,   KC_SCLN, KC_BSPC, \
+  XXXXXXX, HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_G,                     KC_M,    HOME_N,  HOME_E,  HOME_I, HOME_O,  KC_QUOT, \
+  XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   HY_ESC,  KC_TAB,  KC_K,    KC_H,    KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT, \
+                             XXXXXXX, HY_ESC,  LOWER,  KC_SPC,  KC_SPC,  RAISE,   KC_LCTL, KC_RGUI \
 ),
 
 /* QWERTY
