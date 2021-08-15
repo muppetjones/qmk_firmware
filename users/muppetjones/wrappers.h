10c662a27e978b444c841eab9ed4be8b3e3e26f4
Stephen J. Bush <muppetjones@gmail.com>
2021-08-15 14:24:04 -0500

    Wrapper and keymap updates

--- users/muppetjones/wrappers.h
+++ users/muppetjones/wrappers.h
@@ -18,21 +18,6 @@
 #include "muppetjones.h"
 #include "tapmods.h"
 
-/*  Pattern adapted from users/drashna/wrapper.h
-    Define per-layout keymap sections. Also requires a wrapper.
-
-    Example:
-
-        #define LAYOUT_:name_wrapper(...)            LAYOUT_:name(__VA_ARGS__)
-
-        const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-        [_MODS] = LAYOUT_:name _wrapper(
-            _______, ___________________BLANK___________________, ...
-            ...
-        ),
-        }
-*/
-
 #ifdef MOUSEKEY_ENABLE
 #    define MK_WH_D KC_WH_D
 #    define MK_WH_U KC_WH_U
@@ -42,23 +27,59 @@
 #endif
 
 // clang-format off
-/* Blank-ish */
+/* Blank-ish
+ * ,----------------------------------. * ,----------------------------------.
+ * | TRNS | TRNS | TRNS | TRNS | TRNS | * | xxxx | xxxx | xxxx | xxxx | xxxx |
+ * `----------------------------------' * `----------------------------------'
+ * ,----------------------------------. * ,----------------------------------.
+ * | LGUI | LALT | LCTL | LSFT | TRNS | * | TRNS | RSFT | RCTL | LALT | RGUI |
+ * `----------------------------------' * `----------------------------------'
+*/
 #define __BLANK____________________________________ _______, _______, _______, _______, _______
 #define __BLANK_NOOP_______________________________ XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define __BLANK_W_GACS_____________________________ KC_LGUI, KC_LALT, KC_LALT, KC_LSFT, _______
+#define __BLANK_W_GACS_____________________________ KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, _______
 #define __BLANK_W_SCAG_____________________________ _______, KC_RSFT, KC_RCTL, KC_LALT, KC_RGUI
 
-/* Adjust */
-// NOTE: The "BACKLIT" keycode is planck specific
+/* Adjust LH
+ * ,----------------------------------. ,----------------------------------.
+ * |Reset |Debug | xxxx |Term+ |Term- | | xxxx |AGNORM|AGSWAP| xxxx | xxxx |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * |RgbTog| Hue+ | Sat+ | Val+ |RgbMod| | xxxx |CLMKDH|QWERTY| xxxx | xxxx |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * | xxxx | Hue- | Sat- | Val- | xxxx | | xxxx | xxxx | xxxx | xxxx | xxxx |
+ * `----------------------------------' `----------------------------------'
+ */
+ // NOTE: The "BACKLIT" keycode is planck specific
 #define __ADJUST_L1________________________________ RESET,   DEBUG,   XXXXXXX, TERM_ON, TERM_OFF
 #define __ADJUST_L2________________________________ RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, RGB_MOD
 #define __ADJUST_L3________________________________ XXXXXXX, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX
 
+/* Adjust RH (typically use MEDIA) */
 #define __ADJUST_R1________________________________ XXXXXXX, AG_NORM, AG_SWAP, XXXXXXX, XXXXXXX
 #define __ADJUST_R2________________________________ XXXXXXX, CLMK_DH, QWERTY,  XXXXXXX, XXXXXXX
 #define __ADJUST_R3________________________________ __BLANK_NOOP_______________________________
 
-/* COLEMAK mod-DH */
+/* Colemak mod-DH
+ * ,----------------------------------. ,----------------------------------.
+ * |   Q  |   W  |   F  |   P  |   B  | |   J  |   L  |   U  |   Y  |   ;ˆ |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * |   A° |   R° |   S° |   T° |   G  | |   M  |   N° |   E° |   I° |   O° |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * |   Z  |   X  |   C  |   D  |   V˜ | |   K  |   H  |   ,  |   .  |   /  |
+ * `----------------------------------' `----------------------------------'
+ *      ˆ Alternate: KC_QUOT
+ *      ° Alternate: Home row mods (GASC, SCAG)
+ *      ˜ Alternate: Hold for shift
+ *
+ * Colemak mod-DH (alt and alt+shift)
+ * ,----------------------------------. ,----------------------------------.
+ * | œ  Œ | ∑  „ |      | π  ∏ |    ı | | ∆  Ô |      |      | ¥  Á | æ  Æ |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * | å  Å | ®  ‰ | ß  Í | †  ˇ | ©  ˝ | | µ  Â | ˜  ˜ | ´  ´ | ˆ  ˆ |    Ø |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * | Ω  ¸ | ≈  ˛ | ç  Ç |    Î | √  ◊ | | ˚   |    Ó | ≤  ¯ | ≥  ˘ | ÷  ¿ |
+ * `----------------------------------' `----------------------------------'
+ */
 #define __COLEMAK_MOD_DH_L1________________________ KC_Q,    KC_W,    KC_F,     KC_P,   KC_B
 #define __COLEMAK_MOD_DH_L2_W_GACS_________________ HR_A,    HR_R,    HR_S,     HR_T,   KC_G
 #define __COLEMAK_MOD_DH_L3________________________ KC_Z,    KC_X,    KC_C,     KC_D,   KC_V
@@ -69,17 +90,31 @@
 #define __COLEMAK_MOD_DH_R2_W_SCAG_________________ KC_M,    HR_N,    HR_E,     HR_I,   HR_O
 #define __COLEMAK_MOD_DH_R3________________________ KC_K,    KC_H,    KC_COMM,  KC_DOT, KC_SLASH
 
-/* Guitar
- * ,----------------------------------.
- * | Pg Up| Next | Prev | Brm+ | Vol+ |
- * |------+------+------+------+------|
- * | Pg Dn| Play | Stop | Brm- | Vol- |
- * |------+------+------+------+------|
- * | xxxx | xxxx | xxxx | xxxx | xxxx |
- * `----------------------------------'
+/* Function (4 columns)
+ * ,---------------------------.
+ * |  F1  |  F2  |  F3  |  F4  |
+ * |------+------+------+------|
+ * |  F5  |  F6  |  F7  |  F8  |
+ * |------+------+------+------|
+ * |  F9  |  F10 |  F11 |  F12 |
+ * `---------------------------'
  */
-//      _______, _______, _______, _______, _______, _______
-// Not sure why, but this does not work
+#define __FUNC_X1_________________________ KC_F1,   KC_F2,   KC_F3,   KC_F4
+#define __FUNC_X2_________________________ KC_F5,   KC_F6,   KC_F7,   KC_F8
+#define __FUNC_X3_________________________ KC_F9,   KC_F10,  KC_F11,  KC_F12
+
+
+/* Guitar
+ * ,----------------------------------. ,----------------------------------.
+ * |  E   |  F   |• F♯  |  G   |• G♯  | |  A   |• A♯  |  B   |• C   |  C♯  |
+ * |  A   |  A♯  |• B   |  C   |• C♯  | |  D   |• D♯  |  E   |• F   |  F♯  |
+ * |  D   |  D♯  |• E   |  F   |• F♯  | |  G   |• G♯  |  A   |• A♯  |  B   |
+ * |  G   |  G♯  |• A   |  A♯  |• B   | |  C   |• C♯  |  D   |• D♯  |  E   |
+ * |  B   |  C   |• C♯  |  D   |• D♯  | |  E   |• F   |  F♯  |• G   |  G♯  |
+ * |  E   |  F   |• F♯  |  G   |• G♯  | |  A   |• A♯  |  B   |• C   |  C♯  |
+ * `----------------------------------' `----------------------------------'
+ */
+// Not sure why, but this does not work (possibly due to the lowercase 's'?)
 // #ifdef MIDI_ADVANCED
 // #define __GUITAR_1E_L______________________________ MI_E_1,  MI_F_1,  MI_Fs_1, MI_G_1,  MI_Gs_1
 // #define __GUITAR_2A_L______________________________ MI_A_1,  MI_As_1, MI_B_1,  MI_C_2,  MI_Cs_2
@@ -96,14 +131,13 @@
 // #define __GUITAR_6E_R______________________________ MI_A_3,  MI_As_3, MI_B_3,  MI_C_4,  MI_Cs_4
 // #endif
 
-
 /* MEDIA
  * ,----------------------------------.
- * | Pg Up| Next | Prev | Brm+ | Vol+ |
+ * | AuOn | MiOn | MuOn | Brm+ | Vol+ |
  * |------+------+------+------+------|
- * | Pg Dn| Play | Stop | Brm- | Vol- |
+ * | AuOff| MiOff| MuOff| Brm- | Vol- |
  * |------+------+------+------+------|
- * | xxxx | xxxx | xxxx | xxxx | xxxx |
+ * | Play | Stop | Next | Prev | Mute |
  * `----------------------------------'
  */
 // What it MUV_IN and MUV_DE (5C2A and B)?
@@ -111,7 +145,6 @@
 #define __MEDIA_R1_________________________________ AU_ON,   MI_ON,   MU_ON,   KC_BRMU, KC_VOLU
 #define __MEDIA_R2_________________________________ AU_OFF,  MI_OFF,  MU_OFF,  KC_BRMD, KC_VOLD
 #define __MEDIA_R3_________________________________ KC_MPLY, KC_MSTP, KC_MNXT, KC_MPRV, KC_MUTE
-
 
 /* NAV
  * ,----------------------------------.
@@ -126,16 +159,16 @@
 #define __NAV_R2___________________________________ KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT
 #define __NAV_R3___________________________________ __BLANK_NOOP_______________________________
 
-/* NUMPAD
- * ,----------------------------------.
- * | Bksp |  7 & |  8 * |  9 ( |  \ | |
- * |------+------+------+------+------|
- * |  - _ |  4 $ |  5 % |  6 ^ |  *   |
- * |------+------+------+------+------|
- * |  = + |  1 ! |  2 @ |  3 # |  |   |
- * |------+------+------+------+------|
- * |      |  0 ) |  . > |      |      |
- * `----------------------------------'
+/* NUMPAD * (alt, alt+shift)
+ * ,----------------------------------. * ,----------------------------------.
+ * | Del  |  7 & |  8 * |  9 ( |  \ | | * |      | ¶  ‡ | •  ° | ª  · | «  » |
+ * |------+------+------+------+------| * |------+------+------+------+------|
+ * |  - _ |  4 $ |  5 % |  6 ^ |  *   | * | –  — | ¢  › | ∞  ﬁ | §  ﬂ | °  ° |
+ * |------+------+------+------+------| * |------+------+------+------+------|
+ * |  = + |  1 ! |  2 @ |  3 # |  ,   | * | ≠  ± | ¡  ⁄ | ™  € | £  ‹ | ≤  ¯ |
+ * |------+------+------+------+------| * |------+------+------+------+------|
+ * |      |  0 ) |  . > |      |      | * |      |      | º  ‚ |      |      |
+ * `----------------------------------' * `----------------------------------'
  */
 #define __NUMPAD_R1________________________________ KC_DEL,  KC_7,    KC_8,    KC_9,    KC_BSLS
 #define __NUMPAD_R2________________________________ KC_MINS, HR_4,    HR_5,    HR_6,    KC_ASTR
@@ -144,7 +177,15 @@
 
 #define __NUMPAD_R3_ALT____________________________ KC_0,    KC_1,    KC_2,    KC_3,    KC_EQ
 
-/* QWERTY */
+/* Qwerty
+ * ,----------------------------------. ,----------------------------------.
+ * |   Q  |   W  |   E  |   R  |   T  | |   Y  |   U  |   I  |   O  |   P  |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * |   A  |   S  |   D  |   F  |   G  | |   H  |   J  |   K  |   L  |   ;  |
+ * |------+------+------+------+------| |------+------+------+------+------|
+ * |   Z  |   X  |   C  |   V  |   B  | |   N  |   M  |   ,  |   .  |   /  |
+ * `----------------------------------' `----------------------------------'
+ */
 #define __QWERTY_L1________________________________ KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
 #define __QWERTY_L2________________________________ KC_A,    KC_S,    KC_D,    KC_F,    KC_G
 #define __QWERTY_L3________________________________ KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
@@ -167,8 +208,10 @@
 #define __SYMBOLS_L3_______________________________ XXXXXXX, XXXXXXX, KC_LCBR, KC_RCBR, KC_LSFT
 
 /* VIM
-#define __BLANK____________________________________ _______, _______, _______, _______, _______
- * Roll through :wq
+ * -- Roll through ":wq" via transparency
+ * ,----------------------------------.
+ * |   Q  |   W  |   :  |      |      |
+ * `----------------------------------'
  */
 #define __VIM_L1___________________________________ _______, _______, KC_COLN, _______, _______
 
